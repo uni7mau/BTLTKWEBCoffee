1bb30914fb9183ba50147a846f577b2b22835765
uni7mau <hoangnamunicorn2208@gmail.com>
2024-10-21 17:16:06 +0700

    new

---
+++ scripts/tintucscript.js
@@ -0,0 +1,12 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.mobile-sidebar')
+
+    sidebar.style.left = '0'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.mobile-sidebar')
+
+    sidebar.style.left = '0'
+}
+
