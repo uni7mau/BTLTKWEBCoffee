7b9281f0595f3efa54d94e3957937d8cb6fdecfd
uni7mau <hoangnamunicorn2208@gmail.com>
2024-10-22 00:47:42 +0700

    all's done except email validation

--- scripts/tintucscript.js
+++ scripts/tintucscript.js
@@ -1,16 +1,37 @@
-function showSidebar() {
-    const mobileIcon = document.getElementById("mb-menu-i")
-    mobileIcon.classList.add("active")
+var mobileIcon = document.getElementById("mb-menu-i")
+var mobileIconActive = 0
+const sidebar = document.querySelector(".mobile-sidebar")
 
-    const sidebar = document.querySelector('.mobile-sidebar')
-
-    sidebar.style.left = '0'
+function changeSidebar() {
+    if (mobileIconActive == 1) {
+        mobileIcon.classList.add("active")
+        sidebar.style.left = '0'
+        mobileIconActive = 0
+    } else {
+        mobileIcon.classList.remove("active")
+        sidebar.style.left = '-300px'
+        mobileIconActive = 1
+    }
 }
 
-function hideSidebar() {
-    const sidebar = document.querySelector('.mobile-sidebar')
-    sidebar.classList.remove('active')
-
-    sidebar.style.left = '-300px'
+function toogleSubMenu(button) {
+    button.nextElementSibling.classList.toggle('active')
+    button.classList.toggle('active')
 }
 
+const input = document.querySelector("#email"),
+    submitBtn = document.querySelector("#emailSubmit")
+
+    input.addEventListener("keyup", ()=>{
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{3}$/
+        if (input.value === "") {
+            submitBtn.classList.remove("active")
+            return submitBtn.style.opacity = 0.2
+        }
+        if (input.value.match(pattern)) {
+            submitBtn.classList.add("active")
+            return submitBtn.style.opacity = 1
+        }
+        submitBtn.classList.remove("active")
+        submitBtn.style.opacity = 0.2
+    })
