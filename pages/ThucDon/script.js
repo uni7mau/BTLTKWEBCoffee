d2de2ff383c3af958586edf03daf5ed432e3a8c1
uni7mau <hoangnamunicorn2208@gmail.com>
2024-10-25 21:30:44 +0700

    update

--- pages/ThucDon/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.menu a').on('click', function(event) {
-        event.preventDefault();
-        var target = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 600); // Thời gian cuộn (600ms)
-    });
-});
--- scripts/khtvscript.js
+++ scripts/khtvscript.js
@@ -2,3 +2,26 @@ function toogleSubMenu(button) {
     button.nextElementSibling.classList.toggle('active')
     button.classList.toggle('active')
 }
+
+let images = [...document.querySelectorAll(".carousel-item")]
+let currImg = 1;
+let currPos = 0;
+
+function show(pos) {
+    let target = document.querySelector(".carousel-pics")
+    if (pos == 1) target. 
+    
+
+}
+
+function prevImg() {
+    currImg -= 1;
+    if (currImg == 0) currImg = images.length
+    show(currImg)
+}
+
+function nextImg() {
+    currImg -= 1;
+    if (currImg == 0) currImg = images.length
+    show(currImg)
+}
