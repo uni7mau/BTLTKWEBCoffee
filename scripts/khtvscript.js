37c07d5afd5f4b0149ad855a34e474134f08c9ff
uni7mau <hoangnamunicorn2208@gmail.com>
2024-10-26 23:31:38 +0700

    update

--- scripts/khtvscript.js
+++ scripts/khtvscript.js
@@ -2,7 +2,6 @@ function toogleSubMenu(button) {
     button.nextElementSibling.classList.toggle('active')
     button.classList.toggle('active')
 }
-
 let images = [...document.querySelectorAll(".carousel-item")]
 let idcts = [...document.querySelectorAll(".item")]
 let currImg = 0;
@@ -19,15 +18,32 @@ function show(pos) {
     images[pos].style.display = "block"
     idcts[pos].classList.add("active")
 }
-
 function prevImg() {
     currImg -= 1;
     if (currImg < 0) currImg = images.length - 1
     show(currImg)
 }
-
 function nextImg() {
     currImg += 1;
     if (currImg >= images.length) currImg = 0
     show(currImg)
+}
+
+let sections = document.querySelectorAll(".section")
+let navLinks = document.querySelectorAll(".header .nav-cell a")
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 65
+        let height = sec.offsetHeight
+        let id = sec.getAttribute("id")
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active")
+                document.querySelector(".header .nav-cell a[href*=" + id + "]").classList.add("active")
+            })
+        }
+    })
 }
